d925046e91f203726a548c07977943d730477332
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-23 11:15:27 -0400

    Vue du mouton

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] parametres) {
 
+		VueMouton.launch(VueMouton.class, parametres);
 		
 	}
 
---
+++ src/VueMouton.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VueMouton extends Application{
+
+	@Override
+	public void start(Stage stade) throws Exception {
+		
+		stade.show();
+		
+	}
+
+}
