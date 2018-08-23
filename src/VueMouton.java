1d7bcc3204de1f592230af1ca3e1657ae573ce07
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-23 11:44:29 -0400

    Exposer les cases d'affichage en tant que membre

--- src/VueMouton.java
+++ src/VueMouton.java
@@ -7,25 +7,30 @@ import javafx.stage.Stage;
 
 public class VueMouton extends Application{
 
+	protected Label valeurNom;
+	protected Label valeurCouleur;
+	protected Label valeurPoids;
+	protected Label valeurNaissance;
+	
 	@Override
 	public void start(Stage stade) throws Exception {
 		Pane panneau = new Pane();	
 		GridPane grilleMouton = new GridPane();
 
 		// https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/GridPane.html
-		Label valeurNom = new Label("Dolly");
+		valeurNom = new Label("Dolly");
 		grilleMouton.add(new Label("Nom : "), 0, 0);
 		grilleMouton.add(valeurNom, 1, 0);
 		
-		Label valeurCouleur = new Label("Grise");
+		valeurCouleur = new Label("Grise");
 		grilleMouton.add(new Label("Couleur : "), 0, 1);
 		grilleMouton.add(valeurCouleur, 1, 1);
 
-		Label valeurPoids = new Label("20 kg");
+		valeurPoids = new Label("20 kg");
 		grilleMouton.add(new Label("Poids : "), 0, 2);
 		grilleMouton.add(valeurPoids, 1, 2);		
 
-		Label valeurNaissance = new Label("5 juin 2015");
+		valeurNaissance = new Label("5 juin 2015");
 		grilleMouton.add(new Label("Naissance : "), 0, 3);
 		grilleMouton.add(valeurNaissance, 1, 3);				
 			
@@ -35,6 +40,7 @@ public class VueMouton extends Application{
 		
 		//// TEST ////
 		Mouton mouton = new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015");
+		this.afficherMouton(mouton); // Appel de ma fonction avant de la programmer (pour tester à mesure)
 		
 	}
 	
