8718addabc6f3578d9250e5da60e76795671bd0d
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-23 11:17:56 -0400

    Pour ajouter des composants a la scene (panneau)

--- src/VueMouton.java
+++ src/VueMouton.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class VueMouton extends Application{
@@ -6,6 +8,9 @@ public class VueMouton extends Application{
 	@Override
 	public void start(Stage stade) throws Exception {
 		
+		Pane panneau = new Pane();
+	
+		stade.setScene(new Scene(panneau, 400, 400));
 		stade.show();
 		
 	}
