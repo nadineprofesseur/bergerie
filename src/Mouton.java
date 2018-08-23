d53596822e7606eafb8c405413009eaf9b232ee3
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-23 11:37:18 -0400

    Modele mouton

---
+++ src/Mouton.java
@@ -0,0 +1,34 @@
+
+public class Mouton {
+
+	protected String nom;
+	protected String couleur;
+	protected String naissance;
+	protected String poids;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getCouleur() {
+		return couleur;
+	}
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+	public String getNaissance() {
+		return naissance;
+	}
+	public void setNaissance(String naissance) {
+		this.naissance = naissance;
+	}
+	public String getPoids() {
+		return poids;
+	}
+	public void setPoids(String poids) {
+		this.poids = poids;
+	}
+	
+}
