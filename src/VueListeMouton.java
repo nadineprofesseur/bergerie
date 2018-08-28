055c72ae6465cc5a6d74104e3f6b4776437f4010
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-28 11:11:32 -0400

    Test de la partie publique de la vue préparé avant le code de l'affichage (encapsulation ou interface publique)

--- src/VueListeMouton.java
+++ src/VueListeMouton.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
@@ -6,14 +9,32 @@ import javafx.stage.Stage;
 
 public class VueListeMouton extends Application {
 
+	protected GridPane grilleMoutons;
+	
 	@Override
 	public void start(Stage stade) throws Exception {
 		Pane panneau = new Pane();	
-		GridPane grilleMoutons = new GridPane();
+		grilleMoutons = new GridPane();
+		
 		
 		panneau.getChildren().add(grilleMoutons);
 		stade.setScene(new Scene(panneau, 400, 400));
 		stade.show();		
+		
+		/// TEST ///
+		List listeMoutonsTest = new ArrayList<Mouton>();
+		listeMoutonsTest.add(new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015"));
+		listeMoutonsTest.add(new Mouton("Molly", "Rousse", "20 kg", "5 mai 2016"));
+		listeMoutonsTest.add(new Mouton("Arthurus", "Noire", "20 kg", "5 mars 2017"));
+		listeMoutonsTest.add(new Mouton("Cheese", "Jaune", "20 kg", "5 septembre 2015"));
+		this.afficherListeMouton(listeMoutonsTest); // Appel de ma fonction avant de la programmer (pour tester à mesure)
+		
+		
+	}
+	
+	public void afficherListeMouton(List<Mouton> listeMoutons)
+	{
+		
 	}
 
 }
