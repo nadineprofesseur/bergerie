7a658d2849acc5db8e9a42d2db7d329803a8b3ae
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-28 11:25:24 -0400

    Vue du formulaire pour ajouter le mouton + interface publique pour recuperer les valeurs

--- src/App.java
+++ src/App.java
@@ -4,7 +4,8 @@ public class App {
 	public static void main(String[] parametres) {
 
 		//VueMouton.launch(VueMouton.class, parametres);
-		VueListeMouton.launch(VueListeMouton.class, parametres);
+		//VueListeMouton.launch(VueListeMouton.class, parametres);
+		VueAjouterMouton.launch(VueAjouterMouton.class, parametres);
 	}
 
 }
---
+++ src/VueAjouterMouton.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class VueAjouterMouton extends Application{
+
+	protected TextField valeurNom;
+	protected TextField valeurCouleur;
+	protected TextField valeurPoids;
+	protected TextField valeurNaissance;
+	
+	@Override
+	public void start(Stage stade) throws Exception {
+		VBox panneau = new VBox();	
+		GridPane grilleMouton = new GridPane();
+
+		// https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/GridPane.html
+		valeurNom = new TextField();
+		grilleMouton.add(new Label("Nom : "), 0, 0);
+		grilleMouton.add(valeurNom, 1, 0);
+		
+		valeurCouleur = new TextField("");
+		grilleMouton.add(new Label("Couleur : "), 0, 1);
+		grilleMouton.add(valeurCouleur, 1, 1);
+
+		valeurPoids = new TextField("");
+		grilleMouton.add(new Label("Poids : "), 0, 2);
+		grilleMouton.add(valeurPoids, 1, 2);		
+
+		valeurNaissance = new TextField("");
+		grilleMouton.add(new Label("Naissance : "), 0, 3);
+		grilleMouton.add(valeurNaissance, 1, 3);				
+			
+		// Todo : retirer les textes magiques
+		panneau.getChildren().add(new Label("Ajouter un mouton")); // Todo : créer un sous-type de Label ou Text pour les titres
+		panneau.getChildren().add(grilleMouton);
+		panneau.getChildren().add(new Button("Enregistrer"));
+		stade.setScene(new Scene(panneau, 400, 400));
+		stade.show();		
+	}
+	
+	public Mouton demanderMouton()
+	{
+		return null;
+	}
+
+}
