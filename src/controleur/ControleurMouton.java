0dfd6ac1d9cfeb055e3451d384f11419855b4615
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-28 22:19:54 -0400

    Tests factorisés dans le controleur

--- src/controleur/ControleurMouton.java
+++ src/controleur/ControleurMouton.java
@@ -1,20 +1,47 @@
 package controleur;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import modele.Mouton;
 import vue.NavigateurDesVues;
+import vue.VueAjouterMouton;
+import vue.VueListeMouton;
+import vue.VueMouton;
 
 public class ControleurMouton {
 	
 	private NavigateurDesVues navigateur;
-	
+	private VueAjouterMouton vueAjouterMouton = null;
+	private VueListeMouton vueListeMouton = null;
+	private VueMouton vueMouton = null;
+		
 	public ControleurMouton(NavigateurDesVues navigateur)
 	{
 		this.navigateur = navigateur;
-		System.out.println("Initialisation du controleur");
+		this.vueAjouterMouton = navigateur.getVueAjouterMouton();
+		this.vueMouton = navigateur.getVueMouton();
+		this.vueListeMouton = navigateur.getVueListeMouton();
 		
-		this.navigateur.naviguerVersVueListeMouton();
+		System.out.println("Initialisation du controleur");
+				
+		//// TEST ////
+		Mouton mouton = new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015");
+		this.vueMouton.afficherMouton(mouton); // Appel de ma fonction avant de la programmer (pour tester à mesure)					
+		
 		this.navigateur.naviguerVersVueMouton();
-		this.navigateur.naviguerVersVueAjouterMouton();
-
+		
+		/// TEST ///
+		List listeMoutonsTest = new ArrayList<Mouton>();
+		listeMoutonsTest.add(new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015"));
+		listeMoutonsTest.add(new Mouton("Molly", "Rousse", "20 kg", "5 mai 2016"));
+		listeMoutonsTest.add(new Mouton("Arthurus", "Noire", "20 kg", "5 mars 2017"));
+		listeMoutonsTest.add(new Mouton("Cheese", "Jaune", "20 kg", "5 septembre 2015"));
+		this.vueListeMouton.afficherListeMouton(listeMoutonsTest); // Appel de ma fonction avant de la programmer (pour tester à mesure)
+		
+		this.navigateur.naviguerVersVueListeMouton();		
+				
+		//this.navigateur.naviguerVersVueAjouterMouton();
 	}
 
 }
--- src/vue/NavigateurDesVues.java
+++ src/vue/NavigateurDesVues.java
@@ -11,9 +11,9 @@ public class NavigateurDesVues extends Application{
 	
 	private Stage stade;
 	
-	private VueAjouterMouton vueAjouterMouton;
-	private VueListeMouton vueListeMouton;
-	private VueMouton vueMouton;
+	private VueAjouterMouton vueAjouterMouton = null;
+	private VueListeMouton vueListeMouton = null;
+	private VueMouton vueMouton = null;
 	
 	private ControleurMouton controleur;
 	
@@ -22,18 +22,6 @@ public class NavigateurDesVues extends Application{
 		this.vueAjouterMouton = new VueAjouterMouton();
 		this.vueListeMouton = new VueListeMouton();
 		this.vueMouton = new VueMouton();
-		
-		/// TEST ///
-		List listeMoutonsTest = new ArrayList<Mouton>();
-		listeMoutonsTest.add(new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015"));
-		listeMoutonsTest.add(new Mouton("Molly", "Rousse", "20 kg", "5 mai 2016"));
-		listeMoutonsTest.add(new Mouton("Arthurus", "Noire", "20 kg", "5 mars 2017"));
-		listeMoutonsTest.add(new Mouton("Cheese", "Jaune", "20 kg", "5 septembre 2015"));
-		this.vueListeMouton.afficherListeMouton(listeMoutonsTest); // Appel de ma fonction avant de la programmer (pour tester à mesure)
-		
-		//// TEST ////
-		Mouton mouton = new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015");
-		this.vueMouton.afficherMouton(mouton); // Appel de ma fonction avant de la programmer (pour tester à mesure)			
 	}
 	
 
