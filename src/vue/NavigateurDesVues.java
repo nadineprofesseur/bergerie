227ad669763f2b4b5552f66f02f86557f1f0eb38
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-28 22:07:21 -0400

    Fonctions de navigation dans le navigateur

--- src/vue/NavigateurDesVues.java
+++ src/vue/NavigateurDesVues.java
@@ -8,6 +8,8 @@ import modele.Mouton;
 
 public class NavigateurDesVues extends Application{
 	
+	private Stage stade;
+	
 	private VueAjouterMouton vueAjouterMouton;
 	private VueListeMouton vueListeMouton;
 	private VueMouton vueMouton;
@@ -28,13 +30,51 @@ public class NavigateurDesVues extends Application{
 		
 		//// TEST ////
 		Mouton mouton = new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015");
-		this.vueMouton.afficherMouton(mouton); // Appel de ma fonction avant de la programmer (pour tester à mesure)		
+		this.vueMouton.afficherMouton(mouton); // Appel de ma fonction avant de la programmer (pour tester à mesure)			
 	}
 	
+
 	@Override
 	public void start(Stage stade) throws Exception {
+		this.stade = stade;
+		this.stade.setScene(this.vueListeMouton);
+		this.stade.show();
+	
+		//// TEST ////
+		this.naviguerVersVueListeMouton();
+		this.naviguerVersVueMouton();
+		this.naviguerVersVueAjouterMouton();
+
+	}	
+	
+	public VueAjouterMouton getVueAjouterMouton() {
+		return vueAjouterMouton;
+	}
+
+	public VueListeMouton getVueListeMouton() {
+		return vueListeMouton;
+	}
+
+	public VueMouton getVueMouton() {
+		return vueMouton;
+	}
+	
+	public void naviguerVersVueMouton() {
 		stade.setScene(this.vueMouton);
 		stade.show();
 	}
+	
+	public void naviguerVersVueListeMouton()
+	{
+		stade.setScene(this.vueListeMouton);
+		stade.show();		
+	}
+	
+	public void naviguerVersVueAjouterMouton()
+	{
+		stade.setScene(this.vueAjouterMouton);
+		stade.show();				
+	}
+
 	
 }
