29d020f58278c460f1dcaf437952f0b2e191ed4c
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-08-30 11:44:11 -0400

    Simulation MoutonDAO

---
+++ src/accesseur/MoutonDAO.java
@@ -0,0 +1,23 @@
+package accesseur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modele.Mouton;
+
+public class MoutonDAO {
+	
+	private List<Mouton> simulerListerMoutons()
+	{
+		List listeMoutonsTest = new ArrayList<Mouton>();
+		listeMoutonsTest.add(new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015"));
+		listeMoutonsTest.add(new Mouton("Molly", "Rousse", "20 kg", "5 mai 2016"));
+		listeMoutonsTest.add(new Mouton("Arthurus", "Noire", "20 kg", "5 mars 2017"));
+		listeMoutonsTest.add(new Mouton("Cheese", "Jaune", "20 kg", "5 septembre 2015"));
+		return listeMoutonsTest;
+	}
+	public List<Mouton> listerMoutons()
+	{
+		return this.simulerListerMoutons();
+	}
+}
--- src/controleur/ControleurMouton.java
+++ src/controleur/ControleurMouton.java
@@ -3,6 +3,7 @@ package controleur;
 import java.util.ArrayList;
 import java.util.List;
 
+import accesseur.MoutonDAO;
 import modele.Mouton;
 import vue.NavigateurDesVues;
 import vue.VueAjouterMouton;
@@ -35,16 +36,13 @@ public class ControleurMouton {
 		this.navigateur.naviguerVersVueMouton();
 		
 		/// TEST ///
-		List listeMoutonsTest = new ArrayList<Mouton>();
-		listeMoutonsTest.add(new Mouton("Dolly", "Grise", "20 kg", "5 juin 2015"));
-		listeMoutonsTest.add(new Mouton("Molly", "Rousse", "20 kg", "5 mai 2016"));
-		listeMoutonsTest.add(new Mouton("Arthurus", "Noire", "20 kg", "5 mars 2017"));
-		listeMoutonsTest.add(new Mouton("Cheese", "Jaune", "20 kg", "5 septembre 2015"));
+		MoutonDAO moutonDAO = new MoutonDAO();
+		List<Mouton> listeMoutonsTest = moutonDAO.listerMoutons();
 		this.vueListeMouton.afficherListeMouton(listeMoutonsTest); // Appel de ma fonction avant de la programmer (pour tester à mesure)
 		
 		this.navigateur.naviguerVersVueListeMouton();		
 				
-		this.navigateur.naviguerVersVueAjouterMouton();
+		//this.navigateur.naviguerVersVueAjouterMouton();
 	}
 	
 	// SINGLETON DEBUT
