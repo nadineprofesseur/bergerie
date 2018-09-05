7f055c8f98727b46ed82f7c8c379ab45a237cb5f
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-09-05 14:06:16 -0400

    Connection base de données

--- src/donnee/MoutonDAO.java
+++ src/donnee/MoutonDAO.java
@@ -1,5 +1,7 @@
 package donnee;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,24 @@ public class MoutonDAO {
 	}
 	public List<Mouton> listerMoutons()
 	{
+		
+		String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+		String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/bergerie";
+		String BASEDEDONNEES_USAGER = "postgres";
+		String BASEDEDONNEES_MOTDEPASSE = "test";
+		
+		try {
+			Class.forName(BASEDEDONNEES_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return this.simulerListerMoutons();
 	}
 }
