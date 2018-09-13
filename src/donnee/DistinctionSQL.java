3393b0fd156e86aaad81102c5074ff6e64c55d2a
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-09-13 11:30:40 -0400

    [OPTIMAL] static final dans interfaces

--- src/donnee/DistinctionSQL.java
+++ src/donnee/DistinctionSQL.java
@@ -1,5 +1,7 @@
 package donnee;
 
 public interface DistinctionSQL {
-	String SQL_LISTER_DISTINCTIONS_PAR_MOUTON = "SELECT * FROM distinction WHERE mouton = ?";
+	
+	public static final String SQL_LISTER_DISTINCTIONS_PAR_MOUTON = "SELECT * FROM distinction WHERE mouton = ?";
+	
 }
--- src/donnee/MoutonSQL.java
+++ src/donnee/MoutonSQL.java
@@ -2,10 +2,9 @@ package donnee;
 
 public interface MoutonSQL {
 	
-	String SQL_LISTER_MOUTONS = "SELECT * FROM mouton";
-	String SQL_AJOUTER_MOUTON = "INSERT into mouton(nom, couleur, poids, naissance) VALUES(?,?,?,?)";
-	String SQL_MODIFIER_MOUTON = "UPDATE mouton SET nom = ?, couleur = ?, poids = ?, naissance = ? WHERE id = ?";
-	String SQL_RAPPORTER_MOUTON = "SELECT * FROM mouton WHERE id = ?";
-
+	public static final String SQL_LISTER_MOUTONS = "SELECT * FROM mouton";
+	public static final String SQL_AJOUTER_MOUTON = "INSERT into mouton(nom, couleur, poids, naissance) VALUES(?,?,?,?)";
+	public static final String SQL_MODIFIER_MOUTON = "UPDATE mouton SET nom = ?, couleur = ?, poids = ?, naissance = ? WHERE id = ?";
+	public static final String SQL_RAPPORTER_MOUTON = "SELECT * FROM mouton WHERE id = ?";
 
 }
